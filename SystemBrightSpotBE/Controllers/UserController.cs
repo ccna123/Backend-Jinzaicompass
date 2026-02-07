0d978afd37b85032d721140e08c9d9d11e057f30
THANH <95860402+ccna123@users.noreply.github.com>
2026-02-07 17:10:15 +0900

    Update UserController.cs

--- SystemBrightSpotBE/Controllers/UserController.cs
+++ SystemBrightSpotBE/Controllers/UserController.cs
@@ -1013,9 +1013,8 @@ namespace SystemBrightSpotBE.Controllers
                 {
                     BucketName = "jinzaicompass-pdf",
                     Key = key,
-                    //InputStream = stream,
+                    InputStream = stream,
                     ContentType = "application/pdf",
-                    ContentBody = "hello",
                     AutoCloseStream = false,
                     UseChunkEncoding = false,
                     Headers =
